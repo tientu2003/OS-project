6bb662f46176587251ac1450f357792a0c56096c
tientu2003 <117565347+tientu2003@users.noreply.github.com>
2024-01-03 21:14:35 +0700

    update inode

--- Component/inode.h
+++ Component/inode.h
@@ -1,22 +1,15 @@
-#include <bits/stdc++.h>
+#ifndef INODE_H
+#define INODE_H
 // define new type 
-typedef uint32_t __le32;
-typedef uint16_t __le16;
-typedef uint8_t __u8;
-typedef uint64_t __le64;
 
+typedef __UINT32_TYPE__ __le32;
+typedef __UINT16_TYPE__ __le16;
+typedef __UINT8_TYPE__ __u8;
+typedef __UINT64_TYPE__ __le64;
+typedef __UINT32_TYPE__ __u32;
+typedef __UINT32_TYPE__ __u16;
 
-/* data type for block offset of block group */
-typedef int ext4_grpblk_t;
-
-/* data type for filesystem-wide blocks number */
-typedef unsigned long long ext4_fsblk_t;
-
-/* data type for file logical block number */
-typedef uint32_t ext4_lblk_t;
-
-/* data type for block group number */
-typedef unsigned int ext4_group_t;
+#define EXT4_N_BLOCKS 4
 
 #define EXT4_INODE_DIRECT_BLOCK_COUNT 12
 #define EXT4_INODE_INDIRECT_BLOCK EXT4_INODE_DIRECT_BLOCK_COUNT
@@ -26,50 +19,65 @@ typedef unsigned int ext4_group_t;
 #define EXT4_INODE_INDIRECT_BLOCK_COUNT                                        \
 	(EXT4_INODE_BLOCKS - EXT4_INODE_DIRECT_BLOCK_COUNT)
 
-struct ext4_inode {
-	uint16_t mode;		    /* File mode */
-	uint16_t uid;		    /* Low 16 bits of owner uid */
-	uint32_t size_lo;	   /* Size in bytes */
-	uint32_t access_time;       /* Access time */
-	uint32_t change_inode_time; /* I-node change time */
-	uint32_t modification_time; /* Modification time */
-	uint32_t deletion_time;     /* Deletion time */
-	uint16_t gid;		    /* Low 16 bits of group id */
-	uint16_t links_count;       /* Links count */
-	uint32_t blocks_count_lo;   /* Blocks count */
-	uint32_t flags;		    /* File flags */
-	uint32_t unused_osd1;       /* OS dependent - not used in HelenOS */
-	uint32_t blocks[EXT4_INODE_BLOCKS]; /* Pointers to blocks */
-	uint32_t generation;		    /* File version (for NFS) */
-	uint32_t file_acl_lo;		    /* File ACL */
-	uint32_t size_hi;
-	uint32_t obso_faddr; /* Obsoleted fragment address */
-
-	union {
-		struct {
-			uint16_t blocks_high;
-			uint16_t file_acl_high;
-			uint16_t uid_high;
-			uint16_t gid_high;
-			uint16_t checksum_lo; /* crc32c(uuid+inum+inode) LE */
-			uint16_t reserved2;
-		} linux2;
-		struct {
-			uint16_t reserved1;
-			uint16_t mode_high;
-			uint16_t uid_high;
-			uint16_t gid_high;
-			uint32_t author;
-		} hurd2;
-	} osd2;
-
-	uint16_t extra_isize;
-	uint16_t checksum_hi;	/* crc32c(uuid+inum+inode) BE */
-	uint32_t ctime_extra; /* Extra change time (nsec << 2 | epoch) */
-	uint32_t mtime_extra; /* Extra Modification time (nsec << 2 | epoch) */
-	uint32_t atime_extra; /* Extra Access time (nsec << 2 | epoch) */
-	uint32_t crtime;      /* File creation time */
-	uint32_t
-	    crtime_extra;    /* Extra file creation time (nsec << 2 | epoch) */
-	uint32_t version_hi; /* High 32 bits for 64-bit version */
+struct ext4_inode { 
+        __le16  i_mode;         /* File mode */
+        __le16  i_uid;          /* Low 16 bits of Owner Uid */
+        __le32  i_size_lo;      /* Size in bytes */
+        __le32  i_atime;        /* Access time */
+        __le32  i_ctime;        /* Inode Change time */
+        __le32  i_mtime;        /* Modification time */
+        __le32  i_dtime;        /* Deletion Time */
+        __le16  i_gid;          /* Low 16 bits of Group Id */
+        __le16  i_links_count;  /* Links count */
+        __le32  i_blocks_lo;    /* Blocks count */
+        __le32  i_flags;        /* File flags */
+        union {
+                struct {
+                        __le32  l_i_version;
+                } linux1;
+                struct {
+                        __u32  h_i_translator;
+                } hurd1;
+                struct {
+                        __u32  m_i_reserved1;
+                } masix1;
+        } osd1;                         /* OS dependent 1 */
+        __le32  i_block[EXT4_N_BLOCKS];/* Pointers to blocks */
+        __le32  i_generation;   /* File version (for NFS) */
+        __le32  i_file_acl_lo;  /* File ACL */ 
+        __le32  i_size_high;
+        __le32  i_obso_faddr;   /* Obsoleted fragment address */
+        union {
+                struct {
+                        __le16  l_i_blocks_high; /* were l_i_reserved1 */
+                        __le16  l_i_file_acl_high;
+                        __le16  l_i_uid_high;   /* these 2 fields */
+                        __le16  l_i_gid_high;   /* were reserved2[0] */
+                        __le16  l_i_checksum_lo;/* crc32c(uuid+inum+inode) LE */
+                        __le16  l_i_reserved; 
+                } linux2;
+                struct {
+                        __le16  h_i_reserved1;  /* Obsoleted fragment number/size which are removed in ext4 */
+                        __u16   h_i_mode_high;
+                        __u16   h_i_uid_high;
+                        __u16   h_i_gid_high;
+                        __u32   h_i_author;
+                } hurd2;
+                struct {
+                        __le16  h_i_reserved1;  /* Obsoleted fragment number/size which are removed in ext4 */
+                        __le16  m_i_file_acl_high;
+                        __u32   m_i_reserved2[2];
+                } masix2;
+        } osd2;                         /* OS dependent 2 */
+        __le16  i_extra_isize;
+        __le16  i_checksum_hi;  /* crc32c(uuid+inum+inode) BE */
+        __le32  i_ctime_extra;  /* extra Change time      (nsec << 2 | epoch) */
+        __le32  i_mtime_extra;  /* extra Modification time(nsec << 2 | epoch) */
+        __le32  i_atime_extra;  /* extra Access time      (nsec << 2 | epoch) */
+        __le32  i_crtime;       /* File Creation time */
+        __le32  i_crtime_extra; /* extra FileCreationtime (nsec << 2 | epoch) */
+        __le32  i_version_hi;   /* high 32 bits for 64-bit version */
+        __le32  i_projid;       /* Project ID */
 };
+
+#endif;
